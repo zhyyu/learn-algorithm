6b376e456190bb0765e7dac3cf472eb1ce95bb53
todo <todo@todo.com>
2021-02-19 12:18:06 +0800

    jianzhi to 57

---
+++ src/main/java/com/zhyyu/learn/algorithm/jianzhi/Jianzhi_57_II_和为s的连续正数序列_TODO.java
@@ -0,0 +1,15 @@
+package com.zhyyu.learn.algorithm.jianzhi;
+
+public class Jianzhi_57_II_和为s的连续正数序列_TODO {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int[][] findContinuousSequence(int target) {
+            return new int[][]{{}};
+        }
+    }
+
+}
