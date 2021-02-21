73b826c33a4d33b17b6da4f7f67bcc8ea97dd683
todo <todo@todo.com>
2021-02-21 21:47:23 +0800

    jianzhi to 62

---
+++ src/main/java/com/zhyyu/learn/algorithm/jianzhi/Jianzhi_61_扑克牌中的顺子_TODO.java
@@ -0,0 +1,16 @@
+package com.zhyyu.learn.algorithm.jianzhi;
+
+public class Jianzhi_61_扑克牌中的顺子_TODO {
+
+    public static void main(String[] args) {
+        
+    }
+
+    static class Solution {
+        public boolean isStraight(int[] nums) {
+            // TODO: 2021/2/21  
+            return false;
+        }
+    }
+
+}
---
+++ src/main/java/com/zhyyu/learn/algorithm/jianzhi/Jianzhi_62_圆圈中最后剩下的数字_TODO.java
@@ -0,0 +1,16 @@
+package com.zhyyu.learn.algorithm.jianzhi;
+
+public class Jianzhi_62_圆圈中最后剩下的数字_TODO {
+
+    public static void main(String[] args) {
+        
+    }
+
+    static class Solution {
+        public int lastRemaining(int n, int m) {
+            // TODO: 2021/2/21
+            return -1;
+        }
+    }
+
+}
