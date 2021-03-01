a7e55ad41d39805742a7c7a4d7d433d147e59365
todo <todo@todo.com>
2021-03-01 10:13:24 +0800

    jianzhi to 68

---
+++ src/main/java/com/zhyyu/learn/algorithm/jianzhi/Jianzhi_66_构建乘积数组_TODO.java
@@ -0,0 +1,15 @@
+package com.zhyyu.learn.algorithm.jianzhi;
+
+public class Jianzhi_66_构建乘积数组_TODO {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int[] constructArr(int[] a) {
+            return new int[]{};
+        }
+    }
+
+}
---
+++ src/main/java/com/zhyyu/learn/algorithm/jianzhi/Jianzhi_68_II_二叉树的最近公共祖先.java
@@ -0,0 +1,58 @@
+package com.zhyyu.learn.algorithm.jianzhi;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @see https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/er-cha-shu-de-zui-jin-gong-gong-zu-xian-6fdt7/
+ */
+public class Jianzhi_68_II_二叉树的最近公共祖先 {
+
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        Map<Integer, TreeNode> parent = new HashMap<Integer, TreeNode>();
+        Set<Integer> visited = new HashSet<Integer>();
+
+        public void dfs(TreeNode root) {
+            if (root.left != null) {
+                parent.put(root.left.val, root);
+                dfs(root.left);
+            }
+            if (root.right != null) {
+                parent.put(root.right.val, root);
+                dfs(root.right);
+            }
+        }
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            dfs(root);
+            while (p != null) {
+                visited.add(p.val);
+                p = parent.get(p.val);
+            }
+            while (q != null) {
+                if (visited.contains(q.val)) {
+                    return q;
+                }
+                q = parent.get(q.val);
+            }
+            return null;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
